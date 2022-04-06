c37784fa8a93e033805c75c9adfb8657f1a41eee
Nikylin96 <aponosyonok@yandex.by>
2022-04-06 22:52:43 +0300

    Updates

---
+++ precache-manifest.8c3d6baaedbbaf4da244e8f911f9cda8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59c8257173a4423accc56d4edc742de",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "a3392efdf2eea83afafe",
+    "url": "/homeworks/static/css/main.b8ab904f.chunk.css"
+  },
+  {
+    "revision": "b553852805a37dc6a231",
+    "url": "/homeworks/static/js/2.7d7d14eb.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/homeworks/static/js/2.7d7d14eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3392efdf2eea83afafe",
+    "url": "/homeworks/static/js/main.afda7211.chunk.js"
+  },
+  {
+    "revision": "da4b7769bf2abc57381c",
+    "url": "/homeworks/static/js/runtime-main.7671b2db.js"
+  }
+]);
